23497de828d5ea90e6192b15a8ded3a991e04297
32231021-star <32231021@students.liu.edu.lb>
2026-01-06 20:40:00 +0200

    Update NavBar.js

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,33 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaUserShield } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../Styles/NavBar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="navbar">
-      {/* Left Side */}
+      
       <div className="leftSide">
         <Link to="/cart" className="cart-btn" aria-label="Cart">
           🛒
         </Link>
       </div>
 
-      {/* Right Side */}
-      <div className="rightSide">
-        <Link to="/fruits">Fruits</Link>
-        <Link to="/cosmetics">Cosmetics</Link>
-        <Link to="/breakfast">Breakfast</Link>
-        <Link to="/detergent">Detergent</Link>
-        <Link to="/dryfood">Dry Foods_Pantry</Link>
-        <Link to="/review-order">Contact Us</Link>
+      
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
 
-        {/* Admin Login Icon */}
+    
+      <div className={`rightSide ${menuOpen ? "open" : ""}`}>
+        <Link to="/fruits" onClick={() => setMenuOpen(false)}>Fruits</Link>
+        <Link to="/cosmetics" onClick={() => setMenuOpen(false)}>Cosmetics</Link>
+        <Link to="/breakfast" onClick={() => setMenuOpen(false)}>Breakfast</Link>
+        <Link to="/detergent" onClick={() => setMenuOpen(false)}>Detergent</Link>
+        <Link to="/dryfood" onClick={() => setMenuOpen(false)}>Dry Foods_Pantry</Link>
+        <Link to="/review-order" onClick={() => setMenuOpen(false)}>Contact Us</Link>
+
+       
         <Link
           to="/admin/login"
           className="admin-icon"
           title="Admin Login"
           aria-label="Admin Login"
+          onClick={() => setMenuOpen(false)}
         >
           <FaUserShield />
         </Link>
